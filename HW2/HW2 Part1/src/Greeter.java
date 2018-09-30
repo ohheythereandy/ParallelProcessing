fca4661b9aa3d5a6ad271a01080789a28d2a209a
ohheythereandy <andyjrosas@gmail.com>
2018-09-30 13:54:00 -0700

    added dirs

---
+++ HW2/HW2 Part1/src/Greeter.java
@@ -0,0 +1,14 @@
+/**
+ * Created by HiThereAndy on 9/30/2018.
+ */
+public class Greeter {
+
+    public void greet() {
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args){
+        Greeter greeter = new Greeter();
+
+        greeter.greet();
+    }
+}
