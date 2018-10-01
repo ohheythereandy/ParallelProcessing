5d5d7c5fab562163ab05f30fe995f84d60017eba
ohheythereandy <andyjrosas@gmail.com>
2018-09-30 17:06:14 -0700

    finished basics vid8

--- HW2/HW2 Part1/src/Greeter.java
+++ HW2/HW2 Part1/src/Greeter.java
@@ -7,9 +7,11 @@ public class Greeter {
         greeting.perform();
     }
     public static void main(String[] args){
-        Greeter greeter = new Greeter();
 
+        Greeter greeter = new Greeter();
         HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
         greeter.greet(helloWorldGreeting);
+
+        Greeting myLambdaFunction = () -> System.out.print("Hello world!");
     }
 }
