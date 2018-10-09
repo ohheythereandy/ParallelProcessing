0dc5428c40e91123952669120d2d7d8bafea9174
ohheythereandy <andyjrosas@gmail.com>
2018-10-09 01:26:39 -0700

    completed parallel implementation of matrix multiplication

--- HW3/MatrixMult/src/Main.java
+++ HW3/MatrixMult/src/Main.java
@@ -1,17 +1,24 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Vector;
+import java.util.concurrent.*;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Created by Andy on 10/7/18.
  */
 public class Main {
 
+    private static int numThreads = 1;
+    private static final ExecutorService threadpool = Executors.newFixedThreadPool(numThreads);
 
-
-    public static void main(String[] args){
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         int m, n, p;
-        MatrixMult multiply = new MatrixMult();
+
 
         Scanner keyboard = new Scanner(System.in);
         System.out.println("We are going to multiply Matrix m x n by Matrix n x p to obtain Matrix m x p.");
@@ -22,16 +29,32 @@ public class Main {
         System.out.println("What value of p do you wish to use?");
         p = keyboard.nextInt();
 
+        MatrixMult multiply = new MatrixMult();
+
+        MatrixSet task = new MatrixSet(m,p);
+        long startTime = System.currentTimeMillis();
+        Future future = threadpool.submit(task);
+
+        while(!future.isDone()) {
+            //System.out.println("Waiting to initialize 2d matrix");
+        }
+
+
         float[][] firstMatrix = new float[m][n];
         float[][] secondMatrix = new float[n][p];
-        float[][] resultantMatrix = new float[m][p];
+        float[][] resultantMatrix = (float[][]) future.get();
 
         populateMatrix(firstMatrix);
         populateMatrix(secondMatrix);
 
-        long startTime = System.currentTimeMillis();
         multiply.matmult(firstMatrix, secondMatrix, resultantMatrix, m, n, p);
         long endTime = System.currentTimeMillis();
+
+        long totalTime = endTime - startTime;
+
+        System.out.println("Time with " + numThreads + " thread:" + ((float)totalTime / 1000) + " sec");
+
+        threadpool.shutdown();
 
 
     }
@@ -44,6 +67,27 @@ public class Main {
             for(int j =0 ; j < firstMatrix[i].length; j++){
                 firstMatrix[i][j] = rand.nextInt(100);
             }
+        }
+    }
+
+    private static class MatrixSet implements Callable {
+
+        float[][] initialMatrix;
+        int m, p;
+
+        public MatrixSet(int m, int p) {
+            initialMatrix = new float[m][p];
+            this.m = m;
+            this.p = p;
+        }
+
+        public float[][] call()  {
+
+                IntStream.range(0, m-1).parallel()
+                        .forEach(i -> IntStream.range(0, p-1).forEach(j -> initialMatrix[i][j]=0));
+
+
+            return initialMatrix;
         }
     }
 }
--- HW3/MatrixMult/src/MatrixMult.java
+++ HW3/MatrixMult/src/MatrixMult.java
@@ -1,13 +1,28 @@
+import java.util.Arrays;
+import java.util.Vector;
+
 /**
  * Created by Andy on 10/7/18.
  */
-public class MatrixMult extends Thread{
+public class MatrixMult {
+
+
+    public MatrixMult() {
+
+    }
 
     public void matmult(float[][] firstMatrix, float[][] secondMatrix, float[][] resultantMatrix, int m, int n, int p) {
 
+        for(int i = 0; i < m; i++){
+
+            for(int j = 0; j < p; j++){
+                for(int k =0; k < n; k++){
+                    resultantMatrix[i][j] = resultantMatrix[i][j] + (firstMatrix[i][k] * secondMatrix[k][j]);
+                }
+            }
+
+        }
     }
 
-    public void run() {
 
-    }
 }
